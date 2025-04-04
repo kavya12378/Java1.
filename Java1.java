7638eddace606665a5cad007d429c3ac1da1bf3d
kavya12378 <01fe23bca142@gmail.com>
2025-04-04 10:34:23 +0530

    save

---
+++ Java1.java
@@ -0,0 +1,13 @@
+package JAVA1;
+
+public class Java1 {
+    int n=5;
+    public static void main(String[] args) {
+        if(n%2==0){
+            System.out.println("The number is Even");
+            else
+            System.out.println("The number is Odd");
+        }
+    }
+    
+}
